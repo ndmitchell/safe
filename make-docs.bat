b28325615ecfd080031c44d3e0dc00ed4b82a69a
Neil Mitchell <ndmitchell@gmail.com>
2013-10-11 15:24:23 +0100

    Delete make-docs.bat

--- make-docs.bat
+++
@@ -1,9 +0,0 @@
-mkdir docs
-copy Safe.hs docs\Safe.hs
-haddock -h docs\Safe.hs --odir=docs --source-module=Safe.hs.html --source-entity=Safe.hs.html#%%{NAME}
-
-copy d:\bin\hscolour.css docs\hscolour.css
-hscolour -anchorCSS docs\Safe.hs > docs\Safe.hs.html
-
-if "%1" == "deploy" copy "docs\*.*" "W:\projects\safe\*.*"
-

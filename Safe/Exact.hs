7e1008cbdc22574fe35a8f8ac0201ec295a6ee9f
Neil Mitchell <ndmitchell@gmail.com>
2014-06-26 21:36:30 +0100

    Add a module of Exact functions

--- Safe/Exact.hs
+++ Safe/Exact.hs
@@ -1,47 +1,93 @@
+{- |
+Provides functions that raise errors in corner cases instead of returning \"best effort\"
+results, then provides wrappers like the "Safe" module. As some examples:
+
+* @'takeExact' 3 [1,2]@ raises an error, in contrast to 'take' which would return
+  just two elements.
+
+* @'takeExact' (-1) [1,2]@ raises an error, in contrast to 'take' which would return
+  no elements.
+
+* @'zip' [1,2] [1]@ raises an error, in contrast to 'zip' which would only pair up the
+  first element.
+
+Note that the @May@ variants of these functions are /strict/ in at least the bit of the prefix
+of the list required to spot errors. The standard and @Note@ versions are lazy, but throw
+errors later in the process - they do not check upfront.
+-}
 module Safe.Exact(
-    takeExact,
-    dropExact,
-    splitAtExact
+    -- * New functions
+    takeExact, dropExact, splitAtExact,
+    -- * Safe wrappers
+    takeExactMay, takeExactNote,
+    dropExactMay, dropExactNote,
+    splitAtExactMay, splitAtExactNote,
     ) where
 
+import Control.Arrow
+
+
+---------------------------------------------------------------------
+-- SPECIFICATIONS
 
 -- |
 -- > takeExact n xs =
--- >   | n >= 0 && n <= length xs = Just (take n xs)
--- >   | otherwise                = Nothing
-takeExact :: Int -> [a] -> Maybe [a]
-takeExact 0 [] = Just []
-takeExact n [] = Nothing
-takeExact n (x:xs)
-  | n < 0     = Nothing
-  | n == 0    = Just []
-  | otherwise = fmap (x:) (takeExact (n - 1) xs)
-
+-- >   | n >= 0 && n <= length xs = take n xs
+-- >   | otherwise                = error "some message"
+takeExact :: Int -> [a] -> [a]
+takeExact = takeExactNote ""
 
 -- |
 -- > dropExact n xs =
--- >   | n >= 0 && n <= length xs = Just (drop n xs)
--- >   | otherwise                = Nothing
-dropExact :: Int -> [a] -> Maybe [a]
-dropExact 0 [] = Just []
-dropExact n [] = Nothing
-dropExact n (x:xs)
-  | n < 0     = Nothing
-  | n == 0    = Just (x:xs)
-  | otherwise = dropExact (n - 1) xs
-
+-- >   | n >= 0 && n <= length xs = drop n xs
+-- >   | otherwise                = error "some message"
+dropExact :: Int -> [a] -> [a]
+dropExact = dropExactNote ""
 
 -- |
 -- > splitAtExact n xs =
--- >   | n >= 0 && n <= length xs = Just (splitAt n xs)
--- >   | otherwise                = Nothing
-splitAtExact :: Int -> [a] -> Maybe ([a], [a])
-splitAtExact 0 [] = Just ([], [])
-splitAtExact n [] = Nothing
-splitAtExact n (x:xs)
-  | n < 0     = Nothing
-  | n == 0    = Just ([], x:xs)
-  | otherwise = do (ys, zs) <- splitAtExact (n - 1) xs
-                   return (x:ys, zs)
+-- >   | n >= 0 && n <= length xs = splitAt n xs
+-- >   | otherwise                = error "some message"
+splitAtExact :: Int -> [a] -> ([a], [a])
+splitAtExact = splitAtExactNote ""
 
 
+---------------------------------------------------------------------
+-- IMPLEMENTATIONS
+
+{-# INLINE splitAtExact_ #-}
+splitAtExact_ :: (String -> r) -> ([a] -> r) -> (a -> r -> r) -> Int -> [a] -> r
+splitAtExact_ err nil cons o xs
+    | o < 0 = err $ "index must not be negative, index=" ++ show o
+    | otherwise = f o xs
+    where
+        f 0 xs = nil xs
+        f i (x:xs) = x `cons` f (i-1) xs
+        f i [] = err $ "index too large, index=" ++ show o ++ ", length=" ++ show (o-i)
+
+
+---------------------------------------------------------------------
+-- WRAPPERS
+
+addNote fun note msg = error $
+    "Safe.Exact." ++ fun ++ ", " ++ msg ++ (if null note then "" else ", " ++ note)
+
+takeExactNote :: String -> Int -> [a] -> [a]
+takeExactNote note = splitAtExact_ (addNote "takeExact" note) (const []) (:)
+
+takeExactMay :: Int -> [a] -> Maybe [a]
+takeExactMay = splitAtExact_ (const Nothing) (const $ Just []) (\a -> fmap (a:))
+
+dropExactNote :: String -> Int -> [a] -> [a]
+dropExactNote note = splitAtExact_ (addNote "dropExact" note) id (flip const)
+
+dropExactMay :: Int -> [a] -> Maybe [a]
+dropExactMay = splitAtExact_ (const Nothing) Just (flip const)
+
+splitAtExactNote :: String -> Int -> [a] -> ([a], [a])
+splitAtExactNote note = splitAtExact_ (addNote "splitAtExact" note)
+    (\x -> ([], x)) (\a b -> first (a:) b)
+
+splitAtExactMay :: Int -> [a] -> Maybe ([a], [a])
+splitAtExactMay = splitAtExact_ (const Nothing)
+    (\x -> Just ([], x)) (\a b -> fmap (first (a:)) b)

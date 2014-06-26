1ce95175ffefd3850ac58cbe586461b3e7587243
Neil Mitchell <ndmitchell@gmail.com>
2014-06-26 21:37:11 +0100

    A couple of new utilities

--- Safe/Util.hs
+++ Safe/Util.hs
@@ -17,3 +17,10 @@ liftMay test func val = if test val then Nothing else Just $ func val
 fromNoteModule :: String -> String -> String -> Maybe a -> a
 fromNoteModule modu note func = fromMaybe (error msg)
     where msg = modu ++ "." ++ func ++ (if null note then "" else ", " ++ note)
+
+fromNoteEitherModule :: String -> String -> String -> Either String a -> a
+fromNoteEitherModule modu note func = either (error . msg) id
+    where msg ex = modu ++ "." ++ func ++ " " ++ ex ++ (if null note then "" else ", " ++ note)
+
+eitherToMaybe :: Either a b -> Maybe b
+eitherToMaybe = either (const Nothing) Just

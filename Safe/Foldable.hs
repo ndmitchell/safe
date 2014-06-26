32b5d1a46468d2f4ccf5bbdae7a3d09db1af4bfc
Neil Mitchell <ndmitchell@gmail.com>
2014-06-26 17:02:01 +0100

    Clean up the Foldable module, deprecate the *Safe variants

--- Safe/Foldable.hs
+++ Safe/Foldable.hs
@@ -1,11 +1,15 @@
 {- |
-Equivalent versions to the "Safe" module, but generalised to work
-over any 'Foldable' type.
+Versions of 'Foldable' functions, with wrappers like the "Safe" module.
 -}
 module Safe.Foldable(
-    foldl1Note, foldl1Def, foldl1May, foldl1Safe,
-    foldr1Note, foldr1Def, foldr1May, foldr1Safe,
-    findJust, findJustDef, findJustNote, findJustSafe
+    -- * New functions
+    findJust,
+    -- * Safe wrappers
+    foldl1Note, foldl1Def, foldl1May,
+    foldr1Note, foldr1Def, foldr1May,
+    findJustNote, findJustDef,
+    -- * Deprecated
+    foldl1Safe, foldr1Safe, findJustSafe
     ) where
 
 import Data.Foldable
@@ -17,8 +21,6 @@ mfl :: (a -> a -> a) -> Maybe a -> a -> Maybe a
 mfl _ Nothing x = Just x
 mfl fun (Just y) x = Just (fun y x)
 
--- |
--- > Same as Data.Foldable.foldl1
 foldl1Note :: Foldable t => String -> (a -> a -> a) -> t a -> a
 foldl1Note msg fun fld = fromMaybe (error $ "Safe.Foldable.foldl1Note: empty list, " ++ msg) (foldl (mfl fun) Nothing fld)
 
@@ -28,7 +30,7 @@ foldl1Def def fun fld = fromMaybe def (foldl (mfl fun) Nothing fld)
 foldl1May :: Foldable t => (a -> a -> a) -> t a -> Maybe a
 foldl1May fun = foldl (mfl fun) Nothing
 
--- | Default value is the mempty from a monoid
+{-# DEPRECATED foldl1Safe "Use @foldl f mempty@ instead." #-}
 foldl1Safe :: (Monoid m, Foldable t) => (m -> m -> m) -> t m -> m
 foldl1Safe fun = foldl fun mempty
 
@@ -37,8 +39,6 @@ mfr :: (a -> a -> a) -> a -> Maybe a -> Maybe a
 mfr _ x Nothing = Just x
 mfr fun x (Just y) = Just (fun x y)
 
--- |
--- > Same as Data.Foldable.foldr1
 foldr1Note :: Foldable t => String -> (a -> a -> a) -> t a -> a
 foldr1Note msg fun fld = fromMaybe (error $ "Safe.Foldable.foldr1Note: empty list, " ++ msg) (foldr (mfr fun) Nothing fld)
 
@@ -48,13 +48,12 @@ foldr1Def def fun fld = fromMaybe def (foldr (mfr fun) Nothing fld)
 foldr1May :: Foldable t => (a -> a -> a) -> t a -> Maybe a
 foldr1May fun = foldr (mfr fun) Nothing
 
--- | Default value is the mempty from a monoid
+{-# DEPRECATED foldr1Safe "Use @foldr f mempty@ instead." #-}
 foldr1Safe :: (Monoid m, Foldable t) => (m -> m -> m) -> t m -> m
 foldr1Safe fun = foldr fun mempty
 
 -- |
--- > Same as Data.Foldable.find
-
+-- > findJust op = fromJust . find op
 findJust :: Foldable t => (a -> Bool) -> t a -> a
 findJust op fld = fromMaybe (error "Safe.Foldable.findJust, item not found") (find op fld)
 
@@ -64,8 +63,6 @@ findJustDef def op fld = fromMaybe def (find op fld)
 findJustNote :: Foldable t => String -> (a -> Bool) -> t a -> a
 findJustNote msg op fld = fromMaybe (error $ "Safe.Foldable.findJustNote: element not found, " ++ msg) (find op fld)
 
--- | Default value is the mempty from a monoid
+{-# DEPRECATED findJustSafe "Use @findJustDef mempty@ instead." #-}
 findJustSafe :: (Monoid m, Foldable t) => (m -> Bool) -> t m -> m
-findJustSafe op fld = fromMaybe mempty (find op fld)
-
-
+findJustSafe = findJustDef mempty

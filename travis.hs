492062280fa4226c56a9bd034cacbf964d97b625
Neil Mitchell <ndmitchell@gmail.com>
2014-11-06 11:24:34 +0000

    Requires the deepseq library on GHC 7.2

--- travis.hs
+++ travis.hs
@@ -4,5 +4,5 @@ import Control.Exception.Extra
 
 main :: IO ()
 main = do
-    retry 3 $ system_ "cabal install QuickCheck"
+    retry 3 $ system_ "cabal install QuickCheck deepseq"
     system_ "runhaskell Test"

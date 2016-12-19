299422f35d2c9a3cf3362faa928c0a1eecf993fe
Neil Mitchell <ndmitchell@gmail.com>
2016-12-19 21:27:13 +0000

    Move the test suite into the .cabal file

--- travis.hs
+++
@@ -1,9 +0,0 @@
-
-import System.Process.Extra
-import Control.Exception.Extra
-
-main :: IO ()
-main = do
-    system "cabal install deepseq" -- will fail on GHC 7.4 and above, since already installed, which is fine
-    retry 3 $ system_ "cabal install QuickCheck"
-    system_ "runhaskell Test"

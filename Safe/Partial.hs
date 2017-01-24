2cdbb3fc9a2a2b3600f73cb2f9726b215c33b889
Tebello Thejane <zyxoas@gmail.com>
2017-01-24 07:40:11 +0200

    Use GLASGOW_HASKELL macro instead of MIN_VERSION_base, in concordance with Safe/Foldable.hs.

--- Safe/Partial.hs
+++ Safe/Partial.hs
@@ -7,23 +7,22 @@ module Safe.Partial(
   Partial
   ) where
 
-#if MIN_VERSION_base(4,9,0)
-import GHC.Stack (HasCallStack)
-#elif MIN_VERSION_base(4,8,1)
+#if __GLASGOW_HASKELL__ < 710
+import GHC.Exts (Constraint)
+#elif __GLASGOW_HASKELL__ < 800
 import GHC.Stack (CallStack)
 #else
-import GHC.Exts (Constraint)
+import GHC.Stack (HasCallStack)
 #endif
 
 -- | A constraint synonym which denotes that the function is partial, and will
 --   (on GHC 8.* and up) produce a stack trace on failure.
 --   You may mark your own non-total functions as Partial, if necessary, and this
 --   will ensure that they produce useful stack traces.
-
-#if MIN_VERSION_base(4,9,0)
-type Partial = HasCallStack
-#elif MIN_VERSION_base(4,8,1)
+#if __GLASGOW_HASKELL__ < 710
+type Partial = (() :: Constraint)
+#elif __GLASGOW_HASKELL__ < 800
 type Partial = (?loc :: CallStack)
 #else
-type Partial = (() :: Constraint)
+type Partial = HasCallStack
 #endif
